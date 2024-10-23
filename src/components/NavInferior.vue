f60b06067d2a8289caf38f244cbe9901420843bc
Mailen <maimonney30@gmail.com>
2024-10-23 19:25:52 -0300

    agregado de nav

---
+++ src/components/NavInferior.vue
@@ -0,0 +1,29 @@
+<template>
+    <nav>
+        <div class="flex items-center justify-center min-h-screen antialiased bg-gray-100">
+            <div
+                class="relative flex items-center px-6 overflow-hidden bg-white border-4 border-purple-500 h-28 rounded-2xl">
+                <nav class="flex items-center justify-center gap-8">
+                    <ul>
+                        <li>
+                            <a href="#" class="grid w-16 h-16 grid-cols-1 grid-rows-1">Home</a>
+                        </li>
+                        <li>
+                            <a href="#" class="grid w-16 h-16 grid-cols-1 grid-rows-1">Crear</a>
+                        </li>
+                        <li>
+                            <a href="#" class="grid w-16 h-16 grid-cols-1 grid-rows-1">Perfil</a>
+                        </li>
+                    </ul>
+
+                </nav>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "Navbar",
+};
+</script>
