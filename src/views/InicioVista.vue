f40c4bf241c41634a8a5e1a91762e2b11b55e786
Mailen <maimonney30@gmail.com>
2024-10-29 14:42:20 -0300

    inicio comienzo

--- src/views/InicioVista.vue
+++ src/views/InicioVista.vue
@@ -1,27 +1,36 @@
-<script setup>
-import { ref, onMounted } from 'vue';
-import { fetchVuelos } from '../services/apiService.js';
-
-const vuelos = ref([]);
-
-const getVuelos = async () => {
-  try {
-    const data = await fetchVuelos();
-    vuelos.value = data.vuelosAPI; 
-  } catch (error) {
-    console.error('Error al obtener los vuelos:', error);
-  }
-};
-
-onMounted(() => {
-  getVuelos();
-});
-</script>
-
 <template>
   <main>
     <div class="flex flex-col justify-center items-center">
-      <h1>Inicio</h1>
+      <h1 class="text-xl font-bold">¡Descubre nuevos destinos!</h1>
+      <div class="mb-4">
+        <label for="busqueda" class="block text-sm font-medium">Búsqueda:</label>
+        <input type="text" id="busqueda" class="border border-gray-300 p-2 rounded w-full" placeholder="Buscar destinos" required>
+      </div>
+
+      <div class="w-full">
+        <h2 class="text-lg font-semibold mb-2 ml-2.5">Mí itinerario:</h2>
+
+        <div class="overflow-x-auto whitespace-nowrap">
+          <div class="flex gap-6">
+            <div class="w-[150px] h-[150px] bg-gray-700 rounded-lg shadow-lg">
+              <p class="text-white text-center">Contenido 1</p>
+            </div>
+
+            <div class="w-[150px] h-[150px] bg-gray-700 rounded-lg shadow-lg">
+              <p class="text-white text-center">Contenido 2</p>
+            </div>
+
+            <div class="w-[150px] h-[150px] bg-gray-700 rounded-lg shadow-lg">
+              <p class="text-white text-center">Contenido 3</p>
+            </div>
+
+            <div class="w-[150px] h-[150px] bg-gray-700 rounded-lg shadow-lg">
+              <p class="text-white text-center">Contenido 4</p>
+            </div>
+          </div>
+        </div>
+        
+      </div>
     </div>
   </main>
 </template>
