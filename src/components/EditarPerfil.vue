73f90ed4f42f836ab2dbd450c0c168fb42c43f77
dayayalaa <day1414@hotmail.com>
2025-01-23 18:17:54 -0300

    Se puede subir imagen en editarPerfil.vue

--- src/components/EditarPerfil.vue
+++ src/components/EditarPerfil.vue
@@ -9,7 +9,7 @@ const decodeJWT = (token) => {
     const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
     const jsonPayload = decodeURIComponent(
         atob(base64)
-            .split('') 
+            .split('')
             .map((c) => {
                 return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
             })
@@ -32,8 +32,8 @@ const successMessage = ref('');
 const loading = ref(true);
 const userId = ref(null);
 
-const fotoPerfilPreview = ref(null);  
-const fotoPortadaPreview = ref(null);  
+const fotoPerfilPreview = ref(null);
+const fotoPortadaPreview = ref(null);
 
 const router = useRouter();
 
@@ -41,7 +41,7 @@ const previewFotoPerfil = (event) => {
     const file = event.target.files[0];
     if (file) {
         fotoPerfilPreview.value = URL.createObjectURL(file);
-        fotoPerfil.value = file; 
+        fotoPerfil.value = file;
     }
 };
 
@@ -58,7 +58,7 @@ const fetchUserData = async () => {
 
     try {
         const response = await axios.get(`https://back-tesis-lovat.vercel.app/arcana/usuarios/${userId.value}`, {
-            headers: { Authorization: `Bearer ${token}` }
+            headers: { Authorization: `Bearer ${token}` },
         });
         const usuario = response.data.data;
 
@@ -68,6 +68,8 @@ const fetchUserData = async () => {
             descripcion.value = usuario.descripcion || '';
             telefono.value = usuario.telefono || '';
             provincia.value = usuario.provincia || '';
+            fotoPerfilPreview.value = usuario.fotoPerfil || null;
+            fotoPortadaPreview.value = usuario.fotoPortada || null;
         } else {
             errorMessage.value = 'No se encontró la información del usuario.';
         }
@@ -83,83 +85,106 @@ const provincias = [
     'Bariloche', 'Salta', 'Rosario', 'Tucumán', 'Iguazú', 'Neuquén',
     'Misiones', 'Posadas', 'San Fernando del Valle de Catamarca', 'San Juan',
     'Río Gallegos', 'Río Grande', 'El Calafate', 'San Luis', 'Resistencia',
-    'Tremedal', 'General Roca'
+    'Tremedal', 'General Roca',
 ];
 
-const actualizarPerfil = async () => {
+// Función para actualizar los datos del perfil (sin imágenes)
+const actualizarDatosPerfil = async () => {
     const token = localStorage.getItem('token');
-    const datosAEnviar = new FormData();
-    datosAEnviar.append('nombre', nombre.value);
-    datosAEnviar.append('email', email.value);
-    datosAEnviar.append('descripcion', descripcion.value);
-    datosAEnviar.append('telefono', telefono.value);
-    datosAEnviar.append('provincia', provincia.value);
+    const datosAEnviar = {
+        nombre: nombre.value,
+        email: email.value,
+        descripcion: descripcion.value,
+        telefono: telefono.value,
+        provincia: provincia.value,
+    };
 
     try {
-        const respuesta = await axios.put(`https://back-tesis-lovat.vercel.app/arcana/imagen/updatePerfil/${userId.value}`, datosAEnviar, {
-            headers: {
-                'Content-Type': 'multipart/form-data',
-                Authorization: `Bearer ${token}`,
-            },
-        });
+        const respuesta = await axios.put(
+            `https://back-tesis-lovat.vercel.app/arcana/usuarios/${userId.value}`,
+            datosAEnviar,
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
 
         if (respuesta.status === 200) {
-            successMessage.value = 'Perfil actualizado exitosamente!';
+            successMessage.value = 'Datos del perfil actualizados exitosamente!';
             fetchUserData(); // Refresca los datos del usuario
         }
     } catch (error) {
-        console.error('Error al actualizar el perfil:', error);
-        errorMessage.value = 'Hubo un error al actualizar el perfil.';
+        console.error('Error al actualizar los datos del perfil:', error);
+        errorMessage.value = 'Hubo un error al actualizar los datos del perfil.';
     }
 };
 
-const actualizarPortada = async () => {
+// Función para actualizar la foto de perfil
+
+const actualizarFotoPerfil = async () => {
     const token = localStorage.getItem('token');
     const datosAEnviar = new FormData();
-    if (fotoPortada.value) {
-        datosAEnviar.append('fotoPortada', fotoPortada.value);
+    if (fotoPerfil.value) {
+        datosAEnviar.append('file', fotoPerfil.value);
+    } else {
+        errorMessage.value = 'No se ha seleccionado ninguna imagen.';
+        return;
     }
 
     try {
-        const respuesta = await axios.put(`https://back-tesis-lovat.vercel.app/arcana/imagen/updatePortada/${userId.value}`, datosAEnviar, {
-            headers: {
-                'Content-Type': 'multipart/form-data',
-                Authorization: `Bearer ${token}`,
-            },
-        });
+        const respuesta = await axios.put(
+            `https://back-tesis-lovat.vercel.app/arcana/imagen/updatePerfil/${userId.value}`,
+            datosAEnviar,
+            {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+
+        if (respuesta.status === 200) {
+            successMessage.value = 'Foto de perfil actualizada exitosamente!';
+            fetchUserData();
+        }
+    } catch (error) {
+        console.error('Error al actualizar la foto de perfil:', error.response?.data || error.message);
+        errorMessage.value = 'Hubo un error al actualizar la foto de perfil.';
+    }
+};
+
+const actualizarFotoPortada = async () => {
+    const token = localStorage.getItem('token');
+    const datosAEnviar = new FormData();
+
+    // Agregar foto de portada si existe
+    if (fotoPortada.value) {
+        datosAEnviar.append('file', fotoPortada.value); // Asegúrate de que el campo sea 'file'
+    } else {
+        errorMessage.value = 'No se ha seleccionado ninguna imagen.';
+        return; // Detener la función si no hay archivo
+    }
+
+    try {
+        const respuesta = await axios.put(
+            `https://back-tesis-lovat.vercel.app/arcana/imagen/updatePortada/${userId.value}`,
+            datosAEnviar,
+            {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
 
         if (respuesta.status === 200) {
             successMessage.value = 'Foto de portada actualizada exitosamente!';
             fetchUserData(); // Refresca los datos del usuario
         }
     } catch (error) {
-        console.error('Error al actualizar la portada:', error);
-        errorMessage.value = 'Hubo un error al actualizar la portada.';
-    }
-};
-
-const actualizarFotoPerfil = async () => {
-    const token = localStorage.getItem('token');
-    const datosAEnviar = new FormData();
-    if (fotoPerfil.value) {
-        datosAEnviar.append('fotoPerfil', fotoPerfil.value);
-    }
-
-    try {
-        const respuesta = await axios.put(`https://back-tesis-lovat.vercel.app/arcana/usuarios/updatePerfil/${userId.value}`, datosAEnviar, {
-            headers: {
-                'Content-Type': 'multipart/form-data',
-                Authorization: `Bearer ${token}`,
-            },
-        });
-
-        if (respuesta.status === 200) {
-            successMessage.value = 'Foto de perfil actualizada exitosamente!';
-            fetchUserData(); // Refresca los datos del usuario
-        }
-    } catch (error) {
-        console.error('Error al actualizar la foto de perfil:', error);
-        errorMessage.value = 'Hubo un error al actualizar la foto de perfil.';
+        console.error('Error al actualizar la foto de portada:', error.response?.data || error.message);
+        errorMessage.value = 'Hubo un error al actualizar la foto de portada.';
     }
 };
 
@@ -193,34 +218,46 @@ onMounted(() => {
     <div class="container mx-auto p-6">
         <h2 class="text-2xl font-bold mb-4 text-center">Editar Perfil</h2>
         <div v-if="loading" class="text-center text-gray-500">Cargando...</div>
-        <form v-else @submit.prevent="actualizarPerfil" enctype="multipart/form-data" class="bg-white p-6 shadow-md rounded-lg">
-
+        <form v-else @submit.prevent="actualizarDatosPerfil" enctype="multipart/form-data"
+            class="bg-white p-6 shadow-md rounded-lg">
+            <!-- Campos del formulario -->
             <div class="mb-4">
-                <label for="nombre" class="block text-sm font-medium text-gray-700">Nombre <span class="text-red-500">*</span></label>
-                <input v-model="nombre" type="text" id="nombre" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500" required />
+                <label for="nombre" class="block text-sm font-medium text-gray-700">Nombre <span
+                        class="text-red-500">*</span></label>
+                <input v-model="nombre" type="text" id="nombre"
+                    class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+                    required />
                 <div v-if="!nombre" class="text-red-500 text-sm">El nombre es obligatorio.</div>
             </div>
 
             <div class="mb-4">
-                <label for="email" class="block text-sm font-medium text-gray-700">Email <span class="text-red-500">*</span></label>
-                <input v-model="email" type="email" id="email" autocomplete="username"  class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500" required />
+                <label for="email" class="block text-sm font-medium text-gray-700">Email <span
+                        class="text-red-500">*</span></label>
+                <input v-model="email" type="email" id="email" autocomplete="username"
+                    class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+                    required />
                 <div v-if="!email" class="text-red-500 text-sm">El email es obligatorio.</div>
             </div>
 
             <div class="mb-4">
                 <label for="descripcion" class="block text-sm font-medium text-gray-700">Descripción</label>
-                <textarea v-model="descripcion" id="descripcion" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"></textarea>
+                <textarea v-model="descripcion" id="descripcion"
+                    class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"></textarea>
             </div>
 
             <div class="mb-4">
                 <label for="telefono" class="block text-sm font-medium text-gray-700">Teléfono</label>
-                <input v-model="telefono" type="tel" id="telefono" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500" />
-                <div v-if="telefono && telefono.length < 10" class="text-red-500 text-sm">El teléfono debe tener al menos 10 caracteres.</div>
+                <input v-model="telefono" type="tel" id="telefono"
+                    class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500" />
+                <div v-if="telefono && telefono.length < 10" class="text-red-500 text-sm">El teléfono debe tener al
+                    menos 10 caracteres.</div>
             </div>
 
             <div class="mb-4">
-                <label for="provincia" class="block text-sm font-medium text-gray-700">Provincia <span class="text-red-500">*</span></label>
-                <select v-model="provincia" id="provincia" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500">
+                <label for="provincia" class="block text-sm font-medium text-gray-700">Provincia <span
+                        class="text-red-500">*</span></label>
+                <select v-model="provincia" id="provincia"
+                    class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500">
                     <option value="" disabled>Selecciona una provincia</option>
                     <option v-for="p in provincias" :key="p" :value="p">{{ p }}</option>
                 </select>
@@ -230,36 +267,58 @@ onMounted(() => {
             <div class="mb-4">
                 <label for="fotoPerfil" class="block text-sm font-medium text-gray-700">Foto de Perfil</label>
                 <input type="file" id="fotoPerfil" accept="image/*" @change="previewFotoPerfil" class="mt-1" />
+                <p class="text-sm text-gray-500">El tamaño máximo permitido es de 5 MB.</p>
                 <div v-if="fotoPerfilPreview" class="mt-2">
-                    <img :src="fotoPerfilPreview" alt="Vista previa Foto de Perfil" class="w-32 h-32 object-cover rounded-full" />
+                    <img :src="fotoPerfilPreview" alt="Vista previa Foto de Perfil"
+                        class="w-32 h-32 object-cover rounded-full" />
                 </div>
+                <button @click.prevent="actualizarFotoPerfil"
+                    class="mt-2 bg-blue-500 text-white font-semibold py-2 px-4 rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+                    Actualizar Foto de Perfil
+                </button>
             </div>
 
             <div class="mb-4">
                 <label for="fotoPortada" class="block text-sm font-medium text-gray-700">Foto de Portada</label>
                 <input type="file" id="fotoPortada" accept="image/*" @change="previewFotoPortada" class="mt-1" />
+                <p class="text-sm text-gray-500">El tamaño máximo permitido es de 5 MB.</p>
                 <div v-if="fotoPortadaPreview" class="mt-2">
-                    <img :src="fotoPortadaPreview" alt="Vista previa Foto de Portada" class="w-full h-48 object-cover rounded-lg" />
+                    <img :src="fotoPortadaPreview" alt="Vista previa Foto de Portada"
+                        class="w-full h-48 object-cover rounded-lg" />
                 </div>
+                <button @click.prevent="actualizarFotoPortada"
+                    class="mt-2 bg-blue-500 text-white font-semibold py-2 px-4 rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+                    Actualizar Foto de Portada
+                </button>
             </div>
 
+
+            <!-- Contraseña y Confirmar Contraseña -->
             <div class="mb-4">
                 <label for="contrasenia" class="block text-sm font-medium text-gray-700">Contraseña</label>
-                <input v-model="contrasenia" type="password" id="contrasenia" autocomplete="new-password" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500" />
+                <input v-model="contrasenia" type="password" id="contrasenia" autocomplete="new-password"
+                    class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500" />
             </div>
 
             <div class="mb-4">
-                <label for="confirmarContrasenia" class="block text-sm font-medium text-gray-700">Confirmar Contraseña</label>
-                <input v-model="confirmarContrasenia" type="password" id="confirmarContrasenia"  autocomplete="new-password" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500" />
-                <div v-if="contrasenia && confirmarContrasenia && contrasenia !== confirmarContrasenia" class="text-red-500 text-sm">Las contraseñas no coinciden.</div>
+                <label for="confirmarContrasenia" class="block text-sm font-medium text-gray-700">Confirmar
+                    Contraseña</label>
+                <input v-model="confirmarContrasenia" type="password" id="confirmarContrasenia"
+                    autocomplete="new-password"
+                    class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500" />
+                <div v-if="contrasenia && confirmarContrasenia && contrasenia !== confirmarContrasenia"
+                    class="text-red-500 text-sm">Las contraseñas no coinciden.</div>
             </div>
 
+            <!-- Mensajes de error y éxito -->
             <div v-if="errorMessage" class="text-red-500 text-center mt-4">{{ errorMessage }}</div>
             <div v-if="successMessage" class="text-green-500 text-center mt-4">{{ successMessage }}</div>
 
+            <!-- Botón de Actualizar Perfil -->
             <div class="mt-6 text-center">
-                <button type="submit" class="w-full bg-blue-500 text-white font-semibold py-2 px-4 rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-                    Actualizar Perfil
+                <button type="submit"
+                    class="w-full bg-blue-500 text-white font-semibold py-2 px-4 rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+                    Actualizar Datos del Perfil
                 </button>
             </div>
         </form>
