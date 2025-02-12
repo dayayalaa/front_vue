f9adab7dd062cbaff2ef0fab5a991832f1e9eabe
dayayalaa <day1414@hotmail.com>
2025-02-11 21:38:49 -0300

    se arreglo reserva de tour, puede solo reservar usuario y vincula ahora por provincia la reserva

--- src/views/VistaTur.vue
+++ src/views/VistaTur.vue
@@ -1,136 +1,157 @@
 <script setup>
-import { onMounted, ref, computed } from "vue";
-import { useRoute } from "vue-router";
-import axios from "axios";
-import IrAtras from '../components/IrAtras.vue';
-import TituloTerciario from "../components/TituloTerciario.vue";
+import { ref, onMounted, computed } from 'vue'
+import { useRoute } from 'vue-router'
+import IrAtras from '../components/IrAtras.vue'
+import TituloTerciario from '../components/TituloTerciario.vue'
+import axios from 'axios'
 
-// Simulación de usuario autenticado
-const user = ref({
-  _id: "676ee0ee1b4bbdb2a9ba223b", // ID del usuario (simulado)
-  role: "user", // Rol del usuario
-});
+const tour = ref(null)
+const cantidadPersonas = ref(1)
+const reservaExitosa = ref(false)
+const loading = ref(true)
+const isUser = ref(false)
+const isGuia = ref(false)
+const errorMessage = ref('')
 
-const isUser = computed(() => user.value?.role === 'user');
+const route = useRoute()
+const tourId = route.params.id
 
-const tour = ref({
-  titulo: "",
-  descripcion: "",
-  precio: 0,
-  provincia: "",
-  duracion: "",
-  fechasDisponibles: [],
-  fotoPortada: "",
-  politicaCancelacion: "",
-  guia: {}
-});
-
-const route = useRoute();
-const cantidadPersonas = ref(1); // Cantidad de personas (valor inicial: 1)
-const reservaExitosa = ref(false); // Estado para mostrar la notificación
-
-// Calcular el costo total
-const costoTotal = computed(() => {
-  return tour.value.precio * cantidadPersonas.value;
-});
+const fetchTourData = async () => {
+  try {
+    const response = await fetch(`https://back-tesis-lovat.vercel.app/arcana/tur/${tourId}`);
+    if (!response.ok) {
+      throw new Error('No se pudo encontrar el tour');
+    }
+    const data = await response.json();
+    console.log('Tour data:', data); 
+    tour.value = data;
+  } catch (error) {
+    console.error('Error al obtener los datos del tour:', error);
+    errorMessage.value = 'No se pudo cargar la información del tour. Intenta más tarde.';
+  }
+};
 
 const formattedFecha = computed(() => {
-  if (tour.value.fechasDisponibles.length > 0) {
-    return new Date(tour.value.fechasDisponibles[0]).toLocaleDateString();
+  if (tour.value && tour.value.fechaDisponible) {
+    const date = new Date(tour.value.fechaDisponible);
+    return date.toLocaleDateString('es-AR', { 
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
   }
-  return "No disponible";
+  return ''; 
 });
 
-onMounted(async () => {
-  const tourId = route.params.id;
-  if (!tourId) {
-    console.error("ID no proporcionado.");
-    return;
-  }
+const decodeJWT = (token) => {
+  const base64Url = token.split('.')[1]
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
+  const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+  }).join(''))
 
-  try {
-    const responseTour = await axios.get(`https://back-tesis-lovat.vercel.app/arcana/tur/${tourId}`);
-    if (responseTour.data) {
-      tour.value = responseTour.data;
-      console.log("Tour cargado:", tour.value);
+  return JSON.parse(jsonPayload)
+}
 
-      const guiaId = tour.value.guia;
-      console.log('Id del guia:', guiaId);
-
-      if (guiaId) {
-        const responseGuia = await axios.get(`https://back-tesis-lovat.vercel.app/arcana/usuarios/${guiaId}`);
-
-        if (responseGuia.data) {
-          console.log("Info del guía:", responseGuia.data);
-          console.log("Nombre del guía:", responseGuia.data.data.nombre);
-          tour.value.guia = responseGuia.data.data;
-          console.log("Guía nombre:", tour.value.guia.data.nombre);
-        } else {
-          console.error("No se encontraron datos para el guía.");
-        }
+const checkUserRole = () => {
+  const token = localStorage.getItem('token');
+  console.log('Token:', token);
+  if (token) {
+    try {
+      const decodedToken = decodeJWT(token);
+      console.log('Decoded Token:', decodedToken);
+      if (typeof decodedToken.rols === 'string') {
+        isGuia.value = decodedToken.rols === 'guia';
+        isUser.value = decodedToken.rols === 'user';
+        console.log('isUser:', isUser.value, 'isGuia:', isGuia.value);
       } else {
-        console.error("No hay ID de guía disponible.");
+        console.error('El campo "rols" no es una cadena:', decodedToken.rols);
       }
-    } else {
-      console.error("No se encontraron datos para este tour.");
+    } catch (error) {
+      console.error('Error decodificando el token:', error);
     }
-  } catch (error) {
-    console.error('Error al obtener el tour o guía:', error);
   }
-});
+}
 
-// Función para reservar el tour
+onMounted(() => {
+  fetchTourData()
+  checkUserRole()
+})
+
 const reservarTour = async () => {
-  const tourId = route.params.id;
-  const userId = user.value?._id; // ID del usuario autenticado
+  const token = localStorage.getItem('token');
+  const userId = token ? decodeJWT(token).userId : null; 
 
-  if (!userId) {
-    alert("Debes iniciar sesión para reservar.");
+  console.log("User ID:", userId); 
+  if (!userId || !tour.value._id || !tour.value.fechasDisponibles || cantidadPersonas.value <= 0) {
+    errorMessage.value = 'Por favor, verifica que todos los campos estén completos y válidos.';
     return;
   }
 
   const reserva = {
-    userId: userId,
-    tourId: tourId,
-    fechaTour: tour.value.fechasDisponibles[0], // Usa la primera fecha disponible
-    cantidadPersonas: cantidadPersonas.value, // Cantidad de personas seleccionada
-  };
+  userId: userId,
+  tourId: tour.value._id, 
+  fechaTour: tour.value.fechasDisponibles[0], 
+  cantidadPersonas: cantidadPersonas.value, 
+  destino: tour.value.provincia, 
+};
+
+  console.log("Reserva:", reserva); 
 
   try {
-    const response = await axios.post('https://back-tesis-lovat.vercel.app/arcana/reservastour/tours', reserva);
+    const response = await axios.post(
+      'https://back-tesis-lovat.vercel.app/arcana/reservastour/tours',
+      reserva,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
     console.log("Reserva creada:", response.data);
-    reservaExitosa.value = true; // Mostrar mensaje de éxito
+    reservaExitosa.value = true; 
+    setTimeout(() => {
+      reservaExitosa.value = false;
+    }, 3000); 
   } catch (error) {
     console.error("Error al crear la reserva:", error);
-    alert("Hubo un error al crear la reserva. Inténtalo de nuevo.");
+    errorMessage.value = 'Hubo un error al realizar la reserva. Intenta más tarde.';
   }
 };
+
+
+const costoTotal = computed(() => {
+  if (tour.value && tour.value.precio) {
+    return cantidadPersonas.value * tour.value.precio;
+  }
+  return 0; 
+})
 </script>
 
 <template>
+  <IrAtras />
   <div class="max-w-4xl mx-auto">
-    <div class="relative mb-4">
+    <div v-if="tour" class="relative mb-4">
       <img :src="tour.fotoPortada" :alt="'Foto del tour ' + tour.titulo" class="w-full h-64 object-cover" />
       <div class="absolute inset-x-0 bottom-0 bg-gradient-to-t from-[#fcf9f4] to-transparent opacity-75 h-1/3"></div>
-      <IrAtras class="absolute top-0" />
       <h2 class="absolute bottom-2 left-1/2 transform -translate-x-1/2 text-center text-4xl text-[#3C4A28]">
         {{ tour.titulo }}
       </h2>
     </div>
 
-    <div class="p-6">
+    <div v-if="tour" class="p-6">
       <TituloTerciario>Información</TituloTerciario>
       <p class="text-lg text-gray-700 mb-4">{{ tour.descripcion }}</p>
 
       <p><strong>Guía: </strong>
-        <router-link v-if="tour.guia._id" :to="`/guias/${tour.guia._id}`" class="text-blue-500 hover:underline">
+        <router-link v-if="tour.guia" :to="`/guias/${tour.guia._id}`" class="text-blue-500 hover:underline">
           {{ tour.guia.nombre }}
         </router-link>
         <span v-else>Guía no disponible</span>
       </p>
 
       <p class="text-xl font-bold text-gray-900 mb-4">
-        <span class="text-lg text-gray-500">Precio:</span> ${{ tour.precio }}
+        <span class="text-lg text-gray-500">Precio:</span> ${{ costoTotal }}
       </p>
       <p class="text-lg text-gray-700 mb-4">
         <span class="font-semibold">Provincia:</span> {{ tour.provincia }}
@@ -146,7 +167,7 @@ const reservarTour = async () => {
       </p>
     </div>
 
-    <div v-if="isUser" class="mt-6">
+    <div v-if="isUser && !isGuia" class="mt-6">
       <form @submit.prevent="reservarTour" class="bg-white p-6 rounded-lg shadow-md">
         <h3 class="text-xl font-semibold mb-4">Reservar Tour</h3>
 
@@ -178,24 +199,14 @@ const reservarTour = async () => {
           Reservar
         </button>
 
-        <!-- Mensaje de confirmación -->
         <div v-if="reservaExitosa" class="mt-4 p-4 bg-green-100 border-l-4 border-green-500 text-green-700">
           <p>¡Reserva realizada con éxito!</p>
         </div>
+
+        <div v-if="errorMessage" class="mt-4 p-4 bg-red-100 border-l-4 border-red-500 text-red-700">
+          <p>{{ errorMessage }}</p>
+        </div>
       </form>
-    </div>
+    </div> 
   </div>
 </template>
-
-<style scoped>
-/* Estilos para el mensaje de confirmación */
-.bg-green-100 {
-  background-color: #f0fff4;
-}
-.border-green-500 {
-  border-color: #48bb78;
-}
-.text-green-700 {
-  color: #2f855a;
-}
-</style>
