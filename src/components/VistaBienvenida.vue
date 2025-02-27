9e4f55636a110ba8993727991944eaeefa535697
Mailen <maimonney30@gmail.com>
2025-02-27 15:40:51 -0300

    carga

--- src/components/VistaBienvenida.vue
+++ src/components/VistaBienvenida.vue
@@ -1,11 +1,16 @@
-<script setup>
-</script>
-
 <template>
-  <div class="fixed top-0 left-0 right-0 bottom-0 bg-[#3C4A28] flex justify-center items-center z-50">
+  <div v-if="!isLoaded" class="fixed top-0 left-0 right-0 bottom-0 bg-[#3C4A28] flex justify-center items-center z-50">
     <video class="w-36 h-auto" autoplay muted @ended="onVideoEnded">
       <source src="/carga.mp4" type="video/mp4" />
       Tu navegador no soporta el formato de video.
     </video>
   </div>
 </template>
+
+<script setup>
+  let isLoaded = false;
+
+  const onVideoEnded = () => {
+    isLoaded = true;  
+  };
+</script>
